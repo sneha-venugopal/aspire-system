b028ffb213035f1e745b1cac35794f0d61ea5e0a
SNEHA V <107302558+sneha-venugopal@users.noreply.github.com>
2024-04-04 09:07:31 +0530

    Add files via upload

---
+++ Quiz Application/script.js
@@ -0,0 +1,24 @@
+
+  function homepage() {
+    window.location.href = "homepage.html";
+    return false;
+  }
+
+  function signup() {
+    window.location.href = "signup.html";
+    return false;
+  }
+
+  // function () {
+  //   var fullname = document.getElementById("username").value;
+  //   var email = document.getElementById("email").value;
+  //   var password = document.getElementById("password").value;
+  
+  //   if (fullname.trim() === '' || email.trim() === '' || password.trim() === '') {
+  //     alert("Please fill in all the fields.");
+  //     return false; 
+  //   } else {
+  //     return window.location.href = "homepage.html";
+  //   }
+  // }
+
